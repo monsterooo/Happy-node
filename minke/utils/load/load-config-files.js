0d17ce61b58564b1fc4fc3b2611d95d9bb57b092
zhu.jiang <zhu.jiang@56qq.com>
2019-12-19 10:15:37 +0800

    使用koa调整结构

--- minke/utils/load/load-config-files.js
+++
@@ -1,5 +0,0 @@
-const checkReservedFilename = require('./check-reserved-filename');
-const loadFiles = require('./load-files');
-
-module.exports = (dir, pattern = 'config/**/*.+(js|json)') =>
-  loadFiles(dir, pattern, { useFileNameAsKey: checkReservedFilename });
