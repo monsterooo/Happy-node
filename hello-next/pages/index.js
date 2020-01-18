4241c2afa5e2349b0bafd11c7b73cab31f7e1e55
zhu.jiang <zhu.jiang@56qq.com>
2020-01-19 00:20:57 +0800

    next simple use

---
+++ hello-next/pages/index.js
@@ -0,0 +1,49 @@
+import Layout from '../components/MyLayout';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/tv/[id]" as={`/tv/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+// const PostLink = props => (
+//   <li>
+//     <Link href="/p/[id]" as={`/p/${props.id}`}>
+//       <a>{props.id}</a>
+//     </Link>
+//   </li>
+// )
+
+// const Index = () => (
+//   <Layout>
+//     <h1>My Blogs</h1>
+//     <ul>
+//       <PostLink id="hello-nextjs" />
+//       <PostLink id="learn-nextjs" />
+//       <PostLink id="deploy-nextjs" />
+//     </ul>
+//   </Layout>
+// );
+
+export default Index;
